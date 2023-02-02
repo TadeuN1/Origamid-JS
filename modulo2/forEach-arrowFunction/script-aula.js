20422bfed98c0afd313f653149434b3d135962a3
Tadeu Neres <tadeunrs@gmail.com>
2023-02-02 20:19:18 -0300

    estudo diário

---
+++ modulo2/forEach-arrowFunction/script-aula.js
@@ -0,0 +1,18 @@
+//const imgs = document.querySelectorAll('img');
+//let i = 0;
+//imgs.forEach(function(item, index, array){
+// console.log(item, index, array);
+//});
+
+const titulos = document.getElementsByClassName('titulo');
+  const titulosArray = Array.from(titulos);
+  titulosArray.forEach(function(item, index, array){
+    // console.log(item, index, array);
+  });
+
+
+  const imgs = document.querySelectorAll('img');
+
+imgs.forEach((item, index) => {
+  console.log (item, index);
+});
