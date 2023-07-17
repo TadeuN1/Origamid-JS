9977cda9d4acc88a7b91559ecd347a8b3a4c0829
Tadeu Neres <tadeunrs@gmail.com>
2023-07-17 16:07:12 -0300

    correction activities

--- modulo4/buttonCreator/script.js
+++ modulo4/buttonCreator/script.js
@@ -5,49 +5,49 @@ controles.addEventListener('change', handleChange);
 
 const handleStyle = {
   element: btn,
-  backgroundColor(value){
+  backgroundColor(value) {
     this.element.style.backgroundColor = value;
   },
-  height(value){
+  height(value) {
     this.element.style.height = value + 'px';
   },
-  width(value){
+  width(value) {
     this.element.style.width = value + 'px';
   },
-  texto(value){
+  texto(value) {
     this.element.innerText = value;
   },
-  color(value){
-    this.element.style.height = value;
+  color(value) {
+    this.element.style.color = value;
   },
-  border(value){
+  border(value) {
     this.element.style.border = value;
   },
-  borderRadius(value){
+  borderRadius(value) {
     this.element.style.borderRadius = value + 'px';
   },
-  fontFamily(value){
+  fontFamily(value) {
     this.element.style.fontFamily = value;
   },
-  fontSize(value){
+  fontSize(value) {
     this.element.style.fontSize = value + 'rem';
   },
 }
 
-
-function handleChange(event){
+function handleChange(event) {
   const name = event.target.name;
   const value = event.target.value;
+
   handleStyle[name](value);
   saveValues(name, value);
   showCss();
 }
 
-function saveValues(name, value){
+function saveValues(name, value) {
   localStorage[name] = value;
 }
 
-function setValues(){
+function setValues() {
   const properties = Object.keys(localStorage);
   properties.forEach(propertie => {
     handleStyle[propertie](localStorage[propertie]);
@@ -58,6 +58,6 @@ function setValues(){
 
 setValues();
 
-function showCss(){
-  cssText.innerHTML = btn.style.cssText.split('; ').join(';</span><span>');
+function showCss() {
+  cssText.innerHTML = '<span>' + btn.style.cssText.split('; ').join(';</span><span>');
 }
