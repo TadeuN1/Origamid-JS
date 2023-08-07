9faa4211148db9225d0f85988fbdac00092f3768
Tadeu Neres <tadeunrs@gmail.com>
2023-08-07 15:44:37 -0300

    regexp lesson

--- modulo8/regexpMetodos/script.js
+++ modulo8/regexpMetodos/script.js
@@ -1,16 +1,38 @@
-/* const regexp = /\w+/gi;
+const regexp = /\w+/gi;
 const regexp1 = new RegExp('\\d+', 'gi');
 
 
 const frase = 'JavaScript Linguagem 101';
 
-console.log(frase.replace(regexp1, 'X')); */
+console.log(frase.replace(regexp1, 'X'));
 
 const frase = 'JavaScript, TypeScript, CoffeScript, Java';
 
-const regexp = /\w+/g;
-let regexpResult;
+const regexp = /Script/g;
 
-while((regexpResult = regexp.exec(frase)) !== null){
-  console.log(regexpResult);
-};
+const resultados = frase.split(regexp);
+
+console.log(resultados); 
+
+const tags = `
+<ul>
+  <li>Item 1</li>
+  <li>Item 2</li>
+</ul>`
+
+const regexp = /(?<=<\/?)\w+/g;
+
+const resultado = tags.replace(regexp, 'div');
+console.log(resultado);
+
+const emails = `
+empresa@email.com
+contato@email.com
+suporte@email.com
+`;
+
+const regexp = /(\w+@)[\w.]+/g;
+
+const resultado = emails.replace(regexp, '$1gmail.com');
+
+console.log(resultado);
